c1d67bf215dde784534df75b778f78111f742346
Alex Zen <108775988+azen12@users.noreply.github.com>
2022-09-15 16:07:37 -0400

    Add files via upload

---
+++ fascism.js
@@ -0,0 +1,13 @@
+let button = document.getElementById('testing');
+
+function unblocker() {
+    document.getElementById('closed').style.display = 'block';
+    setTimeout(alerts, 250);
+};
+
+function alerts() {
+    alert("that nigga gay!!!!");
+}
+
+button.addEventListener('click', unblocker);
+
