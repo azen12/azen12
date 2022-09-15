b4bdf89763e3efce59303864cea3fb12c774c703
Alex Zen <108775988+azen12@users.noreply.github.com>
2022-09-15 15:33:10 -0400

    Update fascism.js

---
+++ fascism.js
@@ -0,0 +1,12 @@
+let button = document.getElementById('testing');
+
+function unblocker() {
+    document.getElementById('closed').style.display = 'block';
+    setTimeout(alerts, 250);
+};
+
+function alerts() {
+    alert("that nigga gay!!!!");
+}
+
+button.addEventListener('click', unblocker);
